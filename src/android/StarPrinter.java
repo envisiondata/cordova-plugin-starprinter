2139ca58e4f0a40203296d82b923e760edffb51f
James Vaught <jvaught@propulsionlabs.com>
2015-02-24 14:52:09 -0600

    plugin name

--- src/android/StarPrinter.java
+++ src/android/StarPrinter.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.device;
+package com.star.printer;
 
 import java.util.TimeZone;
 import java.io.UnsupportedEncodingException;
