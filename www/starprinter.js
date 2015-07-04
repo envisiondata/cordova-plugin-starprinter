ee6ba9d61e09a5be3c993418a1730a19d4626788
James <envisiondata@yahoo.com>
2015-07-04 14:00:22 -0500

    js and iOS

--- www/starprinter.js
+++ www/starprinter.js
@@ -1,7 +1,33 @@
-function StarPrinterPlugin() {}
+cordova.define("com.star.printer.StarPrinter", function (require, exports, module) {
+    function MyPrinter() { }
 
-StarPrinterPlugin.prototype.CheckStatus = function(id, success, error) {
-  cordova.exec(success, error, 'StarPrinter', 'CheckStatus', [id]);
-};
+    MyPrinter.prototype.CheckStatus = function (id, success, error) {
+        //cordova.exec(success, error, 'Calendar', 'CheckStatus', [id]);
+        cordova.exec(
+          success, // success callback function
+          error, // error callback function
+          'StarPrinter', // mapped to our native Java class called "Calendar"
+          'CheckStatus', // with this action name
+          [id]                 // and this array of custom arguments to create our entry
+      );
+    };
+    MyPrinter.prototype.CheckFirmwareVersion = function (id, success, error) {
+        cordova.exec(success, error, 'StarPrinter', 'CheckFirmwareVersion', [id]);
+    };
+    MyPrinter.prototype.PrintSampleReceipt = function (id, success, error) {
+        cordova.exec(success, error, 'StarPrinter', 'PrintSampleReceipt', [id]);
+    };
+    MyPrinter.prototype.PrintInvoice = function (invoice, sig, invoiceDetail, success, error) {
+    cordova.exec(success, error, 'StarPrinter', 'PrintInvoice', 
+         [{                  // and this array of custom arguments to create our entry
+             "invoice": invoice,
+             "sig": sig,
+             "invoiceDetail": invoiceDetail
+         }]);
+    };
+    MyPrinter.prototype.PrintSignature = function (id, success, error) {
+        cordova.exec(success, error, 'StarPrinter', 'PrintImage', id);
+    };
 
-module.exports = new StarPrinterPlugin();
+module.exports = new MyPrinter();
+});
