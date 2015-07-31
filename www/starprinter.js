9ed711bcb4b0223c124ae962d8cafff058c0781e
James <envisiondata@yahoo.com>
2015-07-31 18:35:10 -0500

    merge conflicts

--- www/starprinter.js
+++ www/starprinter.js
@@ -1,7 +1,7 @@
 cordova.define("com.star.printer.StarPrinter", function (require, exports, module) {
     function MyPrinter() { }
 
-<<<<<<< .mine
+
     MyPrinter.prototype.CheckStatus = function (id, success, error) {
         //cordova.exec(success, error, 'Calendar', 'CheckStatus', [id]);
         cordova.exec(
@@ -29,7 +29,6 @@ cordova.define("com.star.printer.StarPrinter", function (require, exports, modul
         cordova.exec(success, error, 'StarPrinter', 'PrintImage', id);
     };
 
-=======
     MyPrinter.prototype.CheckStatus = function (id, success, error) {
         //cordova.exec(success, error, 'Calendar', 'CheckStatus', [id]);
         cordova.exec(
@@ -40,24 +39,6 @@ cordova.define("com.star.printer.StarPrinter", function (require, exports, modul
           [id]                 // and this array of custom arguments to create our entry
       );
     };
-    MyPrinter.prototype.CheckFirmwareVersion = function (id, success, error) {
-        cordova.exec(success, error, 'StarPrinter', 'CheckFirmwareVersion', [id]);
-    };
-    MyPrinter.prototype.PrintSampleReceipt = function (id, success, error) {
-        cordova.exec(success, error, 'StarPrinter', 'PrintSampleReceipt', [id]);
-    };
-    MyPrinter.prototype.PrintInvoice = function (invoice, sig, invoiceDetail, success, error) {
-    cordova.exec(success, error, 'StarPrinter', 'PrintInvoice', 
-         [{                  // and this array of custom arguments to create our entry
-             "invoice": invoice,
-             "sig": sig,
-             "invoiceDetail": invoiceDetail
-         }]);
-    };
-    MyPrinter.prototype.PrintSignature = function (id, success, error) {
-        cordova.exec(success, error, 'StarPrinter', 'PrintImage', id);
-    };
->>>>>>> .theirs
 
 module.exports = new MyPrinter();
 });
