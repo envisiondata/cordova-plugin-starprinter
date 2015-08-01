015a32968c5d587a00e7f725322423e082b09dc2
James <envisiondata@yahoo.com>
2015-08-01 09:39:16 -0500

    starprinter.js

--- www/starprinter.js
+++ www/starprinter.js
@@ -1,44 +1,23 @@
 cordova.define("com.star.printer.StarPrinter", function (require, exports, module) {
-    function MyPrinter() { }
-
-
-    MyPrinter.prototype.CheckStatus = function (id, success, error) {
-        //cordova.exec(success, error, 'Calendar', 'CheckStatus', [id]);
-        cordova.exec(
-          success, // success callback function
-          error, // error callback function
-          'StarPrinter', // mapped to our native Java class called "Calendar"
-          'CheckStatus', // with this action name
-          [id]                 // and this array of custom arguments to create our entry
-      );
-    };
+               function StarPrinter() { }
                
-    MyPrinter.prototype.CheckFirmwareVersion = function (id, success, error) {
-        cordova.exec(success, error, 'StarPrinter', 'CheckFirmwareVersion', [id]);
-    };
+               StarPrinter.prototype.CheckStatus = function (id, success, error) {
+               cordova.exec(success, error, 'StarPrinter', 'CheckStatus', [id]);
+               };
                
-    MyPrinter.prototype.PrintSampleReceipt = function (id, success, error) {
-        cordova.exec(success, error, 'StarPrinter', 'PrintSampleReceipt', [id]);
-    };
+               StarPrinter.prototype.CheckFirmwareVersion = function (id, success, error) {
+               cordova.exec(success, error, 'StarPrinter', 'CheckFirmwareVersion', [id]);
+               };
                
-    MyPrinter.prototype.PrintInvoice = function (invoice, sig, invoiceDetail, success, error) {
-               cordova.exec(success, error, 'StarPrinter', 'PrintInvoice',
-                            [invoice,sig,invoiceDetail]);
-    };
-    MyPrinter.prototype.PrintSignature = function (id, success, error) {
-        cordova.exec(success, error, 'StarPrinter', 'PrintImage', id);
-    };
+               StarPrinter.prototype.PrintSampleReceipt = function (id, success, error) {
+               cordova.exec(success, error, 'StarPrinter', 'PrintSampleReceipt', [id]);
+               };
+               
 
-    MyPrinter.prototype.CheckStatus = function (id, success, error) {
-        //cordova.exec(success, error, 'Calendar', 'CheckStatus', [id]);
-        cordova.exec(
-          success, // success callback function
-          error, // error callback function
-          'StarPrinter', // mapped to our native Java class called "Calendar"
-          'CheckStatus', // with this action name
-          [id]                 // and this array of custom arguments to create our entry
-      );
-    };
-
-module.exports = new MyPrinter();
-});
+               StarPrinter.prototype.PrintSignature = function (id, success, error) {
+               alert(id);
+               cordova.exec(success, error, 'StarPrinter', 'PrintSignature', [id]);
+               };
+               
+               module.exports = new StarPrinter();
+               });
